3d4881c4d5061682163ef54cb2b51716a8e571f8
Asha <asha17@brown.edu>
2020-12-09 14:24:41 -0800

    moved assets to make them accessible

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,16 +1,25 @@
 import React from "react";
+import Logo from "/Users/asha/Documents/BlueSquadChallenge/public/logo512.png";
 
 function Home() {
   return (
     <div className="login">
           <div>
-            <h1>Login</h1>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
-            </p>
+            <div className="login-card">
+              <img src={Logo} alt="Logo" />
+              <div className="email">
+              <input class="input" type="text" name="email" placeholder="Email" />
+              </div>
+              <div className="password">
+              <input class="input" type="text" name="password" placeholder="Password" />
+              </div>
+            <button className="login-button">
+              Login
+            </button>
+            <div className="terms-text">
+              Terms of Use––Privacy Policy
+            </div>
+            </div>
         </div>
     </div>
   );
