069a5fc65de57f2f475f8fe345e656ce537a26e6
Asha <asha17@brown.edu>
2020-12-09 23:04:01 -0800

    POST Built & Tested with Different API

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,16 +1,77 @@
-import React from "react";
+import React, { useMemo, useState, useEffect } from "react";
+import Table from "./Table.jsx";
+import axios from "axios";
+import { LineChart, PieChart } from 'react-chartkick'
+import 'chart.js';
 
 function Dashboard() {
+  // data state to store the TV Maze API data. Its initial value is an empty array
+  const info = [
+    {"name":"Missions Started", "data": {"2017-01-01": 3, "2017-01-02": 4,}},
+    {"name":"Missions Completed", "data": {"2017-01-01": 5, "2017-01-02": 3,}}
+  ];
+
+  const [data, setData] = useState([]);
+
+  // Using useEffect to call the API once mounted and set the data
+  useEffect(() => {
+    (async () => {
+      const result = await axios("https://api.tvmaze.com/search/shows?q=snow");
+      setData(result.data);
+    })();
+  }, []);
+
+    /* 
+    - Columns is a simple array right now, but it will contain some logic later on. It is recommended by react-table to memoize the columns data
+    - Here in this example, we have grouped our columns into two headers. react-table is flexible enough to create grouped table headers
+  */
+ const columns = useMemo(
+  () => [
+    {
+      // first group - TV Show
+      Header: "TV Show",
+      // First group columns
+      columns: [
+        {
+          Header: "Type",
+          accessor: "show.type"
+        },
+        {
+          Header: "Name",
+          accessor: "show.name"
+        },
+        {
+          Header: "# started",
+          accessor: "show.language"
+        },
+        {
+          Header: "# completed",
+          accessor: "show.runtime"
+        },
+        {
+          Header: "# rejected",
+          accessor: "show.status"
+        }
+      ]
+    },
+  ],
+  []
+);
+
   return (
-    <div>
-          <div>
-            <h1>About</h1>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
-            </p>
+    <div className="dashboard">
+      <h1>Missions</h1>
+          <div className="activity">
+            <h3>Activity</h3>
+            <div className="linechart">
+            <LineChart data={info} legend="bottom" colors={["#08A0CB", "#6533CB"]}/>
+            </div>
+          </div>
+          <div className="log">
+            <h3>Log</h3>
+            <div className="table">
+            <Table columns={columns} data={data} />
+            </div>
           </div>
     </div>
   );
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,27 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Logo from "../logo512.png";
 import { Link, withRouter } from "react-router-dom";
 
 function Home() {
+  const [auth, setAuth] = useState("");
+  useEffect(() => {
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+      body: JSON.stringify({user: "asha.d.franchi@gmail.com", password: "OdXhiQnx"})
+    };
+    fetch("https://mock-api.bluesquad.co/{{host}}/auth", requestOptions)
+      .then(response => response.text())
+      .then(data => setAuth(data.id))
+      .then(text => console.log(text))
+  }, [])
+  
   return (
     <div className="login">
-            <div className="login-card">
+            <form noValidate autoComplete="off">
               <img className="login-logo" src={Logo} alt="Logo" height="200" width="300"/>
               <div className="wrap-input">
-              <input className="input" type="text" name="email" placeholder="Email" />
+              <input className="input" type="text" name="email" placeholder="Email"/>
               </div>
               <div className="wrap-input">
               <input className="input" type="text" name="password" placeholder="Password" />
               </div>
-              <Link className="" to="/dashboard">
-            <button className="login-button">
+              <Link className="link" to="/dashboard">
+            <button type="submit" className="login-button">
               Login
             </button>
             </Link>
             <div className="terms-text">
               Terms of Use––Privacy Policy
             </div>
-        </div>
+        </form>
+        <div>{auth}</div>
     </div>
   );
 }
